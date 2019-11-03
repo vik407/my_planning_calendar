5522eaf95be060ad9a4087ff8ac0cd9f9a72729d
Victor Hernandez <vik407@gmail.com>
2019-11-03 10:14:06 -0500

    Change to write and create the file

--- core.py
+++ core.py
@@ -7,31 +7,33 @@ with open('batch_planning_data.json') as json_file:
 	data = json.load(json_file)
 	cond = None
 	if(data):
-		print('BEGIN:VCALENDAR')
-		print('VERSION:2.0')
-		print('CALSCALE:GREGORIAN')
+		f = open("holberton.ical", "w+")
+		f.write('BEGIN:VCALENDAR\n')
+		f.write('VERSION:2.0\n')
+		f.write('CALSCALE:GREGORIAN\n')
 		for p in data['data']:
 			if 'start_date' in p:
 				datetime_s_str = datetime.strptime(p['start_date'], "%d-%m-%Y")
 				if 'end_date' in p:
 					datetime_e_str = datetime.strptime(p['end_date'], "%d-%m-%Y")
-				print('BEGIN:VEVENT')
-				print('DTSTART:' +datetime.strftime(datetime_s_str, "%Y%m%d") + "T000000Z")
+				f.write('BEGIN:VEVENT')
+				f.write('DTSTART:' +datetime.strftime(datetime_s_str, "%Y%m%d") + "T000000Z\n")
 				if 'end_date' in p:
-					print('DTEND:' +datetime.strftime(datetime_e_str, "%Y%m%d") + "T235959Z")
-				print('UID:a4du9v2lvgueruc8mjoepkvjbc@google.com')
-				print('ID: ' +p['id'])
+					f.write('DTEND:' +datetime.strftime(datetime_e_str, "%Y%m%d") + "T235959Z\n")
+				f.write('UID:a4du9v2lvgueruc8mjoepkvjbc@google.com\n')
+				f.write('ID: ' +p['id'] +'\n')
 				if 'project_url' in p:
-					print('DESCRIPTION: Project id ' + p['id'] +' Name ' +p['text'] +' Url ' +("https://intranet.hbtn.io" + p['project_url']))
+					f.write('DESCRIPTION: Project id ' + p['id'] +' Name ' +p['text'] +' Url ' +("https://intranet.hbtn.io" + p['project_url']) +'\n')
 				else:
-					print('DESCRIPTION: Project id ' + p['id'] +' Name ' +p['text'])
-				print('LAST-MODIFIED:' +datetime.strftime(today, "%Y%m%dT%H%M00Z"))
-				print('LOCATION: Cali CLO')
-				print('SEQUENCE:0')
-				print('STATUS:CONFIRMED')
-				print('SUMMARY: ' +p['text'])
-				print('CATEGORIES:http://schemas.google.com/g/2005#event')
-				print('END:VEVENT')
-		print('END:VCALENDAR')
+					f.write('DESCRIPTION: Project id ' + p['id'] +' Name ' +p['text'] +'\n')
+				f.write('LAST-MODIFIED:' +datetime.strftime(today, "%Y%m%dT%H%M00Z") +'\n')
+				f.write('LOCATION: Cali CLO\n')
+				f.write('SEQUENCE:0\n')
+				f.write('STATUS:CONFIRMED\n')
+				f.write('SUMMARY: ' +p['text'] +'\n')
+				f.write('CATEGORIES:http://schemas.google.com/g/2005#event\n')
+				f.write('END:VEVENT\n')
+		f.write('END:VCALENDAR')
+		f.close()
 	else:
 		print('ERROR:THE FILE IS EMPTY? - ARE YOU LOGGED IN?')
