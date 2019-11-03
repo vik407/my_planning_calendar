0eee36b7141f3f010b68a63ba196e81f1a9af173
Victor Hernandez <vik407@gmail.com>
2019-11-03 10:24:13 -0500

    Fixed errors in the generated file

--- core.py
+++ core.py
@@ -16,7 +16,7 @@ with open('batch_planning_data.json') as json_file:
 				datetime_s_str = datetime.strptime(p['start_date'], "%d-%m-%Y")
 				if 'end_date' in p:
 					datetime_e_str = datetime.strptime(p['end_date'], "%d-%m-%Y")
-				f.write('BEGIN:VEVENT')
+				f.write('BEGIN:VEVENT\n')
 				f.write('DTSTART:' +datetime.strftime(datetime_s_str, "%Y%m%d") + "T000000Z\n")
 				if 'end_date' in p:
 					f.write('DTEND:' +datetime.strftime(datetime_e_str, "%Y%m%d") + "T235959Z\n")
@@ -33,7 +33,7 @@ with open('batch_planning_data.json') as json_file:
 				f.write('SUMMARY: ' +p['text'] +'\n')
 				f.write('CATEGORIES:http://schemas.google.com/g/2005#event\n')
 				f.write('END:VEVENT\n')
-		f.write('END:VCALENDAR')
+		f.write('END:VCALENDAR\n')
 		f.close()
 	else:
 		print('ERROR:THE FILE IS EMPTY? - ARE YOU LOGGED IN?')
